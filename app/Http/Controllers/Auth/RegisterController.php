311425de3ea9a33a18537f26d1075942e991bb9c
Martin Striednig <m.striednig@gmail.com>
2020-06-01 20:01:55 +0200

    RegisterController extended  with new fields (role, code)

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,8 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'code' => uniqid(),
+            'role' => null,
             'password' => Hash::make($data['password']),
         ]);
     }
