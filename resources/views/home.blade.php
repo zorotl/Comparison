32aad4bd869645f44598f267326207524ed42532
Martin Striednig <m.striednig@gmail.com>
2020-06-01 20:53:05 +0200

    add nav to app.blade and create generally home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,35 @@
 @extends('layouts.app')
 
+@section('sitetitle', 'Home')
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Startseite</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    You are logged in!
+                        @auth
+                            <div>
+                                <p>Hallo {{ auth()->user()->name ?? '' }}</p>
+                            </div>
+                        @endauth
+
+                        @guest
+                            <div>
+                                <p>Hallo Gast :-)</p>
+                            </div>
+                        @endguest
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Comparison | @yield('sitetitle')</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -26,7 +26,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'Comparison') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -35,7 +35,15 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link {{ Request::is('home') ? 'active' : '' }}" href="/home">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ Request::is('select*') ? 'active' : '' }}" href="/select">Auswahl</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link {{ Request::is('evaluate*') ? 'active' : '' }}" href="/evaluate">Auswertung</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -57,6 +65,8 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ url('/user') }}">User-Konto</a>
+                                    <a class="dropdown-item" href="#">Admin</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
