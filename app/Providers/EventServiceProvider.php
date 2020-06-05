f77bf33873e7034e93cbe4fbd052c83b54f998e2
Martin Striednig <m.striednig@gmail.com>
2020-06-05 20:24:05 +0200

    route:list problem, some things checked, all ok

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -14,9 +14,15 @@ class EventServiceProvider extends ServiceProvider
      *
      * @var array
      */
+//    protected $listen = [
+//        Registered::class => [
+//            SendEmailVerificationNotification::class,
+//        ],
+//    ];
+
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        'Illuminate\Auth\Events\Verified' => [
+            'App\Listeners\LogVerifiedUser',
         ],
     ];
 
